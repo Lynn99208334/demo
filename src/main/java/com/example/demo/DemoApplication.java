2b7c51acab7aa8a31b3d5b236099ba82ce0f0f21
Lynn <akimoto11315@gmail.com>
2022-08-05 12:00:57 +0800

    first

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController   //將 RESTful 的功能引入
+public class DemoApplication {
+
+    @RequestMapping("/")   //
+    public String home(){
+        return "Demo2test";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        System.out.println( "Hello World!" );
+    }
+
+}
